28eaad3f4f590bf71bdc7513897c5d65ea9b512c
EJ McVey <ej.mcvey@geopal-solutions.com>
2022-01-26 14:59:19 +0000

    User removePerson action

--- src/components/PersonCard.tsx
+++ src/components/PersonCard.tsx
@@ -1,25 +1,42 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { DeleteIcon } from "@chakra-ui/icons";
+import { Box, Button, Flex, Text } from "@chakra-ui/react";
+import { useDispatch } from "react-redux";
+import { removePerson } from "../redux/personSlice";
 import { PersonState } from "../types";
 
 
 const PersonCard = ({ personInfo }: any) => {
+
+  const dispatch = useDispatch();
+
+
+  const removePersonId = () => {
+    console.log('current', personInfo.name.first);
+    const id = personInfo.id;
+    dispatch(removePerson({ id }));
+  }
+
   return (
 
-    <Box h='250px'  boxShadow='base' rounded='md' my={4}>
+    <Box boxShadow='base' rounded='md' my={4}>
       <Flex>
-        <Flex borderLeftRadius='8px' p={4}  bg='tomato' w='30%' flexDirection='column'>
-          <Box  borderColor='blue'>
-            <img  src={personInfo.picture.medium} alt='User Person Pic' />
-            <Text mt={4} fontSize='xs'>{ personInfo.phone }</Text>
-            <Text mt={2} fontSize='xs'>{ personInfo.nat }</Text>
+        <Flex borderLeftRadius='8px' p={4}  bg='tomato' w='30%' flexDirection='column' alignItems='center'>
+          <Box fontSize='xs' color='white' fontWeight='semibold'>
+            <img width='100%'  src={personInfo.picture.medium} alt='User Person Pic' />
+            <Text mt={4} >{ personInfo.phone }</Text>
+            <Text mt={2} >{ personInfo.nat }</Text>
           </Box>
 
         </Flex>
         <Box p={4} w='100%'>
 
           <Flex justifyContent='space-between' alignItems='center' fontWeight='semibold' color='tomato'>
-            <Text fontSize='xl' >{ `${personInfo.name.first} ${personInfo.name.last}`  }</Text>
-            <Text fontSize='lg' >{ personInfo.dob.age }</Text>
+            <Flex w='50%'  justifyContent='space-between' alignItems='center' >
+              <Text fontSize='xl' >{ `${personInfo.name.first} ${personInfo.name.last}`  }</Text>
+              <Text fontSize='lg' >{ personInfo.dob.age }</Text>
+            </Flex>
+
+            <Button _focus={{boxShadow: "none", outline: 'none'}} onClick={() => removePersonId()} leftIcon={<DeleteIcon />} colorScheme='tomato' variant='ghost'></Button>            
 
           </Flex>
           <Flex flexDirection='column' fontSize='xs'>
